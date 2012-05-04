3f1727087b4a32b109e5c523a9bd48599feae58c
Mitar <mitar.git@tnode.com>
2012-05-04 14:29:22 +0200

    Hook into notifications also from trac admin commandline.

--- publicnotifications/api.py
+++ publicnotifications/api.py
@@ -1,5 +1,6 @@
 from trac.core import *
 from trac.config import Option
+from trac.admin import IAdminCommandProvider
 from trac.env import IEnvironmentSetupParticipant
 from trac.perm import PermissionCache
 from trac.ticket import notification as ticketnotification
@@ -21,6 +22,8 @@ def get_recipients(self, tktid):
 
     (to, cc) = saved_get_recipients(self, tktid)
 
+    self.env.log.debug("PublicNotifications before, to: %s, cc: %s" % (to, cc))
+
     # Can ticket be viewed by an anonymous user?
     if 'TICKET_VIEW' in PermissionCache(self.env, None, self.ticket.resource):
         always_cc = smtp_public_always_cc and build_addresses(smtp_public_always_cc.replace(',', ' ').split()) or []
@@ -28,6 +31,8 @@ def get_recipients(self, tktid):
         for addr in always_cc:
             if addr not in to and addr not in cc:
                 cc.append(addr)
+
+    self.env.log.debug("PublicNotifications after, to: %s, cc: %s" % (to, cc))
 
     return (to, cc)
 
@@ -40,7 +45,7 @@ class PublicNotifications(Component):
         """Email address(es) to always send notifications for public tickets to,
            addresses can be seen by all recipients (Cc:).""")
     
-    implements(IEnvironmentSetupParticipant)
+    implements(IEnvironmentSetupParticipant, IAdminCommandProvider)
 
     def __init__(self):
         global smtp_public_always_cc
@@ -50,6 +55,7 @@ class PublicNotifications(Component):
             if saved_get_recipients is None:
                 saved_get_recipients = ticketnotification.TicketNotifyEmail.get_recipients
                 ticketnotification.TicketNotifyEmail.get_recipients = get_recipients
+            self.env.log.debug("PublicNotifications hooked")
 
         smtp_public_always_cc = self.smtp_public_always_cc
     
@@ -83,3 +89,10 @@ class PublicNotifications(Component):
         """
 
         pass
+
+    # IAdminCommandProvider methods
+
+    def get_admin_commands(self):
+        """Return a list of available admin commands."""
+
+        return []
